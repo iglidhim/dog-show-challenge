aa608616181e8461235cd328a032bec5f2bff14c
Igli Dhima <iglidhim@amazon.com>
2021-04-18 23:59:36 -0400

    Still working on this

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 document.addEventListener('DOMContentLoaded', () => {
 
-})
+    const BASE_URL = `http://localhost:3000/dogs`
+    const tableBody = document.getElementById('table-body')
+    document.addEventListener("click", handleEvents)
+    const dogForm = document.getElementById('dog-form')
+    
+    fetch(BASE_URL)
+    .then(res => res.json())
+    .then(dogs => dogs.forEach(getDog))
+
+    
